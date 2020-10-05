2c4e4e28945d53ecf7e6c0a435050d4dc6e16d2a
Alexandr Abrosimov <mystudyjs11@gmail.com>
2020-10-05 10:44:17 +0700

    Добавил проект в git: 1. Написал функции рендеринга строк на страницу, 2. Разбил по модулям, 3. Провел рефакторинг когда. 4. Забыл всё это коммитить)

---
+++ src/model.js
@@ -0,0 +1,12 @@
+export const model = [
+  {type: 'title', value: 'Hello World from JS'},
+  {type: 'text', value: 'Some text'},
+  {type: 'columns', value: [
+    '11111',
+    '2222',
+    '333355',
+    '5555'
+
+  ]},
+  {type: 'image', value: './assets/banner.png'}
+];
